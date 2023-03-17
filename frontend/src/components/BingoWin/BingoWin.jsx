dfeff96be46e173bcf776c999ed88768fbf9de0c
Elizabeth Czarny <liz.cz91@gmail.com>
2023-03-17 18:00:58 +0100

    removed comments

--- frontend/src/components/BingoWin/BingoWin.jsx
+++ frontend/src/components/BingoWin/BingoWin.jsx
@@ -24,11 +24,6 @@ const BingoWin = () => {
               recycle={false}
               numberOfPieces={700}
               gravity={0.2}
-              burst={true}
-              // confettiSource={{
-              //   x: window.innerWidth / 2,
-              //   y: window.innerHeight / 2,
-              // }}
             />
           </div>
           <div className="message-container">
